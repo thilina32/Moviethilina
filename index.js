9e2445509082d18516ee3fc9f1e54b6fbc4b3198
Thilina kavishan <113406436+thilina32@users.noreply.github.com>
2024-05-14 22:56:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const { Client } = require('whatsapp-web.js'); 
-const client = new Client();
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-client.on('qr', (qr) => { 
-    // Generate and scan this code with your phone 
-    console.log('QR RECEIVED', qr); 
+const client = new Client({
+    authStrategy: new LocalAuth()
 });
 
-client.on('ready', () => { 
-    console.log('Client is ready!'); 
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
 });
 
-client.on('message', msg => { 
-    if (msg.body == '!ping') { 
-        msg.reply('pong'); 
-    } 
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    if(message.body === '!ping') {
+        message.reply('pong');
+    }
 });
 
 client.initialize();
